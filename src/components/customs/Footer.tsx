90fc9e3a8d7fc58f7948eb3210cbe55194efcb6b
Akkharawit Somwong <akk.somwong@gmail.com>
2024-09-23 12:08:49 +0700

    add: Footer.

---
+++ src/components/customs/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+interface FooterProps {}
+
+export const Footer: React.FC<FooterProps> = () => {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center w-full h-[60px] select-none">
+        <nav className="flex justify-between items-center w-full max-w-[1400px] px-4 gap-6">
+          <div className="text-subtitle-sm text-gray-500">
+            <p>© 2024 TripTiaoPao.com. All Rights Reserved.</p>
+          </div>
+        </nav>
+      </section>
+    </>
+  )
+}
